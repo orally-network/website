8315ff925ae43efe45d2645bb2403cc8ca5ba1b6
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-02-22 12:32:20 +0000

    Create animated background and overlay

---
+++ src/Background/Background.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './Background.module.scss';
+
+const Background = () => {
+  return (
+    <div className={styles.background}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+};
+
+export default Background;
